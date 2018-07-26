b3d6f67ea649d48261d55544f83cc73004740a05
wangzhaozhou <13564861621@163.com>
2018-07-26 16:58:55 +0800

    2

---
+++ WZZPod/WZZPod/Tool/PrintString.m
@@ -0,0 +1,17 @@
+//
+//  PrintString.m
+//  WZZPod
+//
+//  Created by wzz on 2018/7/26.
+//  Copyright © 2018年 com.vcredit.kkcredit.qiye.x1. All rights reserved.
+//
+
+#import "PrintString.h"
+
+@implementation PrintString
+
++ (void)printString {
+    NSLog(@"wzz");
+}
+
+@end
